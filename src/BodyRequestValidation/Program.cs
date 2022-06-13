2bf52d443f02e5deb08cd3b23d8300ad917a9003
Tino Hager <tino.hager@nager.at>
2022-06-13 10:33:09 +0200

    add validation logic

--- src/BodyRequestValidation/Program.cs
+++ src/BodyRequestValidation/Program.cs
@@ -1,19 +1,28 @@
+using BodyRequestValidation.Dtos.Person;
 using BodyRequestValidation.Services;
+using BodyRequestValidation.Validators;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IPersonService, InMemoryPersonService>();
 
-// Add services to the container.
+#region Validators
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+ValidatorOptions.Global.LanguageManager.Enabled = false;
+
+builder.Services.AddTransient<IValidator<PersonCreateRequestDto>, PersonCreateValidator>();
+
+#endregion
+
+builder.Services.AddControllers().AddFluentValidation();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
---
+++ src/BodyRequestValidation/Validators/PersonCreateValidator.cs
@@ -0,0 +1,16 @@
+﻿using BodyRequestValidation.Dtos.Person;
+using FluentValidation;
+using FluentValidation.Validators;
+
+namespace BodyRequestValidation.Validators
+{
+    public class PersonCreateValidator : AbstractValidator<PersonCreateRequestDto>
+    {
+        public PersonCreateValidator()
+        {
+            RuleFor(x => x.Firstname).NotEmpty();
+            RuleFor(x => x.Lastname).NotEmpty();
+            RuleFor(x => x.EmailAddress).EmailAddress(EmailValidationMode.AspNetCoreCompatible);
+        }
+    }
+}
